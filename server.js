c5dc7fa46dbec31ac3815850cf267ac1d7adf33a
Naveed Niaz <naveedniaz2005@gmail.com>
2024-09-15 12:32:43 +0530

    lol

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-var http = require('http'),
-    fs = require('fs');
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHead(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8080);
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;  // or whatever port you're using
+
+// Serve static files from the 'images' folder
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
