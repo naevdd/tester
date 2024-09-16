3e39172b1df5b7ca9a9edb3f2a8b21aebd55a6a7
Naveed Niaz <naveedniaz2005@gmail.com>
2024-09-16 14:13:24 +0530

    added features mentioned in exercise-2

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const http = require('http'),
-    fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-http.createServer(function(request, response){   
-    fs.readFile('./index.html', function (err, html) {
-        if (err) {
-            throw err; 
-        }      
-        response.writeHead(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    })
-}).listen(3000);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/exercise-2',(req,res)=>{
+    res.redirect('/exercise-2-redirected');
+});
+
+app.get('/exercise-2-redirected',(req,res)=>{
+    res.send('redirected to this page');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
