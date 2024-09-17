8bb11b3043d9399d6f4e6f5f8b55615ec9eb5626
Naveed Niaz <naveedniaz2005@gmail.com>
2024-09-17 13:53:59 +0530

    3rd exercise

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const ejs = require("ejs");
 const path = require('path');
+const bodyParser = require('body-parser');
+const moment = require('moment-timezone');
+const csvWriter = require('csv-writer').createObjectCsvWriter;
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const csvFilePath = path.join(__dirname, 'search_log.csv');
+
+const writer = csvWriter({
+  path: csvFilePath,
+  header: [
+    {id: 'searchString', title: 'Search String'},
+    {id: 'dateTime', title: 'Date Time (IST)'},
+    {id: 'ipAddress', title: 'IP Address'},
+    {id: 'userAgent', title: 'Browser'}
+  ],
+  append: true 
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.render('index.ejs');
 });
 
 app.get('/exercise-2',(req,res)=>{
@@ -12,7 +34,24 @@ app.get('/exercise-2',(req,res)=>{
 });
 
 app.get('/exercise-2-redirected',(req,res)=>{
-    res.send('redirected to this page');
+    res.send('Exercise 2 is redirected to this page.');
+});
+
+let searchHistory = [];
+
+app.get('/exercise-3', (req, res) => {
+    res.render('./pages/exercise3.ejs', { history: searchHistory });
+});
+
+app.post('/exercise-3', (req, res) => {
+    const query = req.body.string;
+    searchHistory.push(query);
+    res.render('resultpage.ejs', { query });
+});
+
+app.post('/clear-history', (req, res) => {
+    searchHistory = [];
+    res.redirect('/exercise-3');
 });
 
 app.listen(PORT, () => {
